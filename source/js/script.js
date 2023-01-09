aac0a88725e3e288866e8a4ad202c95920be89e8
VladProfitkit <vlad@profitkit.ru>
2023-01-10 03:53:17 +0400

    Мобильная шапка

--- source/js/script.js
+++ source/js/script.js
@@ -7,21 +7,46 @@ $(function() {
     });
 
     //моб. меню:
-    let menuOpenBtn = $('.header__menu-toggle');
+    let body = $('body'),
+        menuOpenBtn = $('.header__menu-toggle--open'),
+        menuCloseBtn = $('.header__menu-toggle--close'),
+        overlay = $('.overlay'),
+        mobileMenu = $('.header__menu-wrapper'),
+        submenuToggles = $('.header__menu-item--parent .header__menu-link');
 
     const openMobileMenu = function() {
-
+        body.addClass('fixed');
+        overlay.addClass('overlay--active');
+        mobileMenu.addClass('header__menu-wrapper--open');
     }
 
     const closeMobileMenu = function() {
-
+        body.removeClass('fixed');
+        overlay.removeClass('overlay--active');
+        mobileMenu.removeClass('header__menu-wrapper--open');
     }
 
     menuOpenBtn.on('click', function() {
-        if ($(this).hasClass('header__menu-toggle--open')) {
-            closeMobileMenu();
-        } else {
-            openMobileMenu();
+        openMobileMenu();
+    });
+
+    menuCloseBtn.on('click', function() {
+        closeMobileMenu();
+    });
+
+    overlay.on('click', function() {
+        closeMobileMenu();  
+    });
+
+    submenuToggles.on('click', function(e) {
+        if ($(window).width() < 1024) {
+            e.preventDefault();
+            
+            let toggle = $(this),
+                submenu = toggle.next('.header__submenu');
+
+            toggle.toggleClass('header__menu-link--submenu-open');
+            submenu.slideToggle();
         }
     });
 });
