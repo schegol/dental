b601760582d76fc67af0f311abd63bb4e234f8a1
VladProfitkit <vlad@profitkit.ru>
2023-01-10 17:09:54 +0400

    Завершение хедера, начало работы над главной

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,26 @@
+//моб. меню:
+let body = $('body'),
+    menuOpenBtn = $('.header__menu-toggle--open'),
+    menuCloseBtn = $('.header__menu-toggle--close'),
+    overlay = $('.overlay'),
+    mobileMenu = $('.header__menu-wrapper'),
+    submenuToggles = $('.header__menu-item--parent .header__menu-link'),
+    submenus = $('.header__submenu');
+
+const openMobileMenu = function() {
+    body.addClass('fixed');
+    overlay.addClass('overlay--active');
+    mobileMenu.addClass('header__menu-wrapper--open');
+}
+
+const closeMobileMenu = function() {
+    body.removeClass('fixed');
+    overlay.removeClass('overlay--active');
+    mobileMenu.removeClass('header__menu-wrapper--open');
+    submenus.hide();
+    submenuToggles.removeClass('header__menu-link--submenu-open');
+}
+
 $(function() {
     //кнопка "наверх":
     let toTopBtn = $('.to-top-btn');
@@ -5,26 +28,6 @@ $(function() {
     toTopBtn.on('click', function () {
         $('html, body').animate({scrollTop: 0}, 1500);
     });
-
-    //моб. меню:
-    let body = $('body'),
-        menuOpenBtn = $('.header__menu-toggle--open'),
-        menuCloseBtn = $('.header__menu-toggle--close'),
-        overlay = $('.overlay'),
-        mobileMenu = $('.header__menu-wrapper'),
-        submenuToggles = $('.header__menu-item--parent .header__menu-link');
-
-    const openMobileMenu = function() {
-        body.addClass('fixed');
-        overlay.addClass('overlay--active');
-        mobileMenu.addClass('header__menu-wrapper--open');
-    }
-
-    const closeMobileMenu = function() {
-        body.removeClass('fixed');
-        overlay.removeClass('overlay--active');
-        mobileMenu.removeClass('header__menu-wrapper--open');
-    }
 
     menuOpenBtn.on('click', function() {
         openMobileMenu();
@@ -39,7 +42,7 @@ $(function() {
     });
 
     submenuToggles.on('click', function(e) {
-        if ($(window).width() < 1024) {
+        if ($(window).width() < 1500) {
             e.preventDefault();
             
             let toggle = $(this),
@@ -49,4 +52,21 @@ $(function() {
             submenu.slideToggle();
         }
     });
+
+    //сброс фокуса со стрелок слайдеров:
+    let sliderBtns = $('.slider-arrows__arrow');
+
+    sliderBtns.on('click', function() {
+        $(this).blur();
+    });
+});
+
+$(window).on('load resize', function() {
+    let mobileMenu = $('.header__menu-wrapper');
+
+    if ($(window).width() >= 1500) {
+        if (mobileMenu.hasClass('header__menu-wrapper--open')) {
+            closeMobileMenu();
+        }
+    }
 });
